2d26f5b11a955acf6cd613a2348c7fd102cfbd8e
Simon Lydell <simon.lydell@gmail.com>
2026-01-04 18:57:45 +0100

    Try running just `echo`

--- example.js
+++
@@ -1,4 +0,0 @@
-console.log("Start");
-setTimeout(() => {
-  console.log("End");
-}, 1000);
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import * as pty from "node-pty";
 
-const ptyProcess = pty.spawn("node", ["example.js"]);
+const ptyProcess = pty.spawn("echo", ["success"]);
 
 ptyProcess.onData((data) => {
   process.stdout.write(data);
