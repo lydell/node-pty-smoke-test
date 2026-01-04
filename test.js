0e285a4f5f1c04a08c24bd6076dbea75f701703b
Simon Lydell <simon.lydell@gmail.com>
2026-01-04 19:01:27 +0100

    Try to fix Windows

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 import * as pty from "node-pty";
 
-const ptyProcess = pty.spawn("node", ["example.js"]);
+const ptyProcess = process.platform === "win32"
+  ? pty.spawn("cmd.exe", ["/d", "/s", "/q", "/c", "node", "example.js"])
+  : pty.spawn("node", ["example.js"]);
 
 ptyProcess.onData((data) => {
   process.stdout.write(data);
